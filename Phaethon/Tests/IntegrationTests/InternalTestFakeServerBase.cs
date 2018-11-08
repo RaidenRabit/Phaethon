f37b718669e320f481951651867caf4240b80f37
RaidenRabit <1062021@ucn.dk>
2018-11-08 14:12:20 +0100

    #38 added test base

---
+++ Phaethon/Tests/IntegrationTests/InternalTestFakeServerBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Tests.IntegrationTests
+{
+    public class InternalTestFakeServerBase
+    {
+        public HttpClient _client;
+        private HttpServer _server;
+
+        public InternalTestFakeServerBase()
+        {
+            var config = new HttpConfiguration();
+            InternalApi.WebApiConfig.Register(config);
+            // additional config ...
+            _server = new HttpServer(config);
+            _client = new HttpClient(_server);
+            _client.BaseAddress = new Uri("http://localhost:64007/");
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
