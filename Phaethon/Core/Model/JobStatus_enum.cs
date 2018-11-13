743fccbc05acc609a0905d14174273c589f7a3f1
RaidenRabit <1062021@ucn.dk>
2018-11-13 10:54:50 +0100

    #10 added Models

---
+++ Phaethon/Core/Model/JobStatus_enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public enum JobStatus_enum
+    {
+        InProgress = 1,
+        AwaitingCustomerApproval = 2,
+        Completed = 3,
+    }
+}
