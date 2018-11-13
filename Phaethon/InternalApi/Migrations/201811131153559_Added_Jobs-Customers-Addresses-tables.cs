50b94aa12f116eeed9012ad2c15c90bc59adbf9b
RaidenRabit <1062021@ucn.dk>
2018-11-13 12:54:52 +0100

    #10 added migration

---
+++ Phaethon/InternalApi/Migrations/201811131153559_Added_Jobs-Customers-Addresses-tables.cs
@@ -0,0 +1,16 @@
+namespace InternalApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Added_JobsCustomersAddressestables : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
