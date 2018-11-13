10e8e42ff0ee9ec3bc03ccea993bb43c56aca201
RaidenRabit <1062021@ucn.dk>
2018-11-13 13:28:02 +0100

    #10 Added controller and dm. Removed unecessary references

---
+++ Phaethon/InternalApi/DataManagement/IDataManagement/IJobDM.cs
@@ -0,0 +1,11 @@
+﻿using Core.Model;
+
+namespace InternalApi.DataManagement.IDataManagement
+{
+    internal interface IJobDm
+    {
+        bool Create(Job job);
+
+        Job Read(int id);
+    }
+}
---
+++ Phaethon/InternalApi/DataManagement/JobDM.cs
@@ -0,0 +1,26 @@
+﻿using Core.Model;
+using InternalApi.DataAccess;
+using InternalApi.DataManagement.IDataManagement;
+
+namespace InternalApi.DataManagement
+{
+    internal class JobDm : IJobDm
+    {
+        private readonly JobDa _jobDa;
+
+        internal JobDm()
+        {
+            _jobDa = new JobDa();
+        }
+
+        public bool Create(Job job)
+        {
+            return _jobDa.Create(job);
+        }
+
+        public Job Read(int id)
+        {
+            return _jobDa.Read(id);
+        }
+    }
+}
