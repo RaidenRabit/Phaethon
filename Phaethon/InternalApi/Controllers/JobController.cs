323952a21901c8b13170b2364badc94fdfd6e3f4
RaidenRabit <1062021@ucn.dk>
2018-11-13 13:28:02 +0100

    #10 Added controller and dm. Removed unecessary references

---
+++ Phaethon/InternalApi/Controllers/JobController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Core.Model;
+using InternalApi.DataManagement;
+using InternalApi.DataManagement.IDataManagement;
+using Newtonsoft.Json;
+
+namespace InternalApi.Controllers
+{
+    [RoutePrefix("Job")]
+    public class JobController : ApiController
+    {
+        private readonly IJobDm _jobDm;
+
+        public JobController()
+        {
+            _jobDm = new JobDm();
+        }
+        
+        [Route("Create")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> Create()
+        {
+            var requestContent = await Request.Content.ReadAsStringAsync();
+            Job job = JsonConvert.DeserializeObject<Job>(requestContent);
+            return Request.CreateResponse(HttpStatusCode.OK, _jobDm.Create(job));
+        }
+
+        [Route("Read")]
+        [HttpGet]
+        public async Task<HttpResponseMessage> Read()
+        {
+            var requestContent = await Request.Content.ReadAsStringAsync();
+            int id = JsonConvert.DeserializeObject<int>(requestContent);
+            return Request.CreateResponse(HttpStatusCode.OK, _jobDm.Read(id));
+        }
+    }
+}
---
+++ Phaethon/InternalApi/DataAccess/JobDa.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using Core.Model;
+
+namespace InternalApi.DataAccess
+{
+    internal class JobDa
+    {
+        internal bool Create(Job job)
+        {
+            using (var db = new DatabaseContext())
+            {
+                if (db.Invoices.Any(i => i.ID == job.ID))
+                    db.Entry(job).State = EntityState.Modified;
+                if (db.Customers.Any(i => i.ID == job.Customer.ID))
+                    db.Entry(job.Customer).State = EntityState.Modified;
+                if (db.Addresses.Any(i => i.ID == job.Customer.Address.ID))
+                    db.Entry(job.Customer.Address).State = EntityState.Modified;
+                db.Jobs.Add(job);
+
+                return db.SaveChanges() > 0;
+            }
+        }
+
+        internal Job Read(int id)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Jobs
+                    .Include(x => x.Customer)
+                    .Include(x => x.Customer.Address)
+                    .SingleOrDefault(x => x.ID == id);
+            }
+        }
+    }
+}
