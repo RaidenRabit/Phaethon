6e7c83d7d4647aa5606f599a47527d54f101cc27
RaidenRabit <1062021@ucn.dk>
2018-11-13 10:54:50 +0100

    #10 added Models

---
+++ Phaethon/Core/Model/Address.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    [Serializable]
+    [DataContract]
+    public class Address
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string City { get; set; }
+
+        [DataMember]
+        public string Street { get; set; }
+
+        [DataMember]
+        public string Number { get; set; }
+        
+        /// <summary>
+        /// Ex: apartment, room, etc.
+        /// </summary>
+        [DataMember]
+        public string Extra { get; set; }
+    }
+}
