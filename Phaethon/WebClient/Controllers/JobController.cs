abc7c7332422d68795bd9b58e7437cc785249afe
RaidenRabit <1062021@ucn.dk>
2018-11-14 13:34:08 +0100

    #10 added external api endpoints

---
+++ Phaethon/WebClient/Controllers/JobController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebClient.Controllers
+{
+    public class JobController : Controller
+    {
+        public JobController()
+        {
+
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("http://localhost:64010/Job/");
+        }
+
+        // GET: Job
+        public ActionResult Index()
+        {
+            return View(_client.getAsync("/ReadAll"));
+        }
+    }
+}
