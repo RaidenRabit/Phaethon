a04e7d2c0e3d4d528df9028c2fc8b28f6236a1ed
RaidenRabit <1062021@ucn.dk>
2018-12-05 11:11:54 +0100

    #47 added migration

---
+++ Phaethon/InternalApi/Migrations/201812050952594_removed DispalyNames from job and customer tables.cs
@@ -0,0 +1,16 @@
+namespace InternalApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class removedDispalyNamesfromjobandcustomertables : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
