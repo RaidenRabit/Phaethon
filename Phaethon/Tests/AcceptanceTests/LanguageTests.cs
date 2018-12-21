a4eee45d5882ab7b392056c797d1f9e7ca0a9b20
RaidenRabit <1062021@ucn.dk>
2018-12-21 12:06:07 +0100

    #60 temp name fix

---
+++ Phaethon/Tests/AcceptanceTests/LanguageTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Tests.AcceptanceTests
+{
+    public class LanguageTests: AcceptanceTestBase
+    {
+        [Test]
+        public void As_A_User_I_Can_Change_The_Website_Language_to_Latvian()
+        {
+            IWebElement btn = _chromeDriver.FindElement(By.Id("ChangeLanguageLv"));
+            Assert.AreEqual(btn.TagName, "div", btn.TagName);
+        }
+    }
+}
