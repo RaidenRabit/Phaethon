5814d66c1c7ad5a205630b9fc84a2bc99c5b80af
Ralf <bubriks@gmail.com>
2019-01-03 15:35:29 +0100

    #62 HSTS implemented

--- Phaethon/WebClient/App_Start/FilterConfig.cs
+++ Phaethon/WebClient/App_Start/FilterConfig.cs
@@ -1,5 +1,5 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using WebClient.App_Start;
 
 namespace WebClient
 {
@@ -7,7 +7,8 @@ namespace WebClient
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new RequireHttpsAttribute());
+            //filters.Add(new RequireHttpsAttribute());
+            filters.Add(new RequireHstsAttribute(31536000) { IncludeSubDomains = true, Preload = true });
             filters.Add(new HandleErrorAttribute());
         }
     }
