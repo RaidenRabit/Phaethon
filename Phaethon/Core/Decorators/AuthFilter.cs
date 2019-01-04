30ee57556727f56831f1e6c6a16130990dd94fe0
RaidenRabit <1062021@ucn.dk>
2019-01-04 16:09:51 +0100

    #61 general auth filter implemented

---
+++ Phaethon/Core/Decorators/AuthFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Core.Decorators
+{
+    public class AuthFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            try
+            {
+                base.OnActionExecuting(actionContext);
+                string requestUrl = actionContext.Request.RequestUri.ToString();
+
+                if (requestUrl.Contains("Login/CreateOrUpdate")) //if you're just trying to register, allow for it to go on
+                    return;
+
+                var userToken = actionContext.Request.Headers.GetValues("UserToken");
+            }
+            catch (Exception)
+            {
+                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+            }
+        }
+        
+    }
+}
